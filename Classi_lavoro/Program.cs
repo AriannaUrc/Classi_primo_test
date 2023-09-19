0099b9e84688b12fa4d2da961b731f834ccdcac2
URCIUOLI.22381 <URCIUOLI.22381@E2-9-PC15>
2023-09-19 12:49:16 +0200

    Aggiungere i file di progetto.

---
+++ Classi_lavoro/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi_lavoro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prodotto oggetto = new Prodotto("test", 99);
+            Console.WriteLine("Nome prodotto: " + oggetto.ReturnNome() + "\nPrezzo prodotto: " + oggetto.ReturnPrezzo());
+        }
+
+    }
+
+    //prodotto con builder con paramentri
+    public class Prodotto
+    {
+        private string nome;
+        private int prezzo;
+
+        public Prodotto(string nome, int prezzo)
+        {
+            this.nome = nome;
+            this.prezzo = prezzo;
+        }
+
+        public string ReturnNome()
+        {
+            return nome;
+        }
+
+        public int ReturnPrezzo()
+        {
+            return prezzo;
+        }
+    }
+
+
+    //prodotto con builder senza paramentri
+    public class Prodotto2
+    {
+        private string nome;
+        private int prezzo;
+
+        public Prodotto2()
+        {
+            string nome = "test";
+            int prezzo = 44;
+
+            this.nome = nome;
+            this.prezzo = prezzo;
+        }
+
+        public string ReturnNome()
+        {
+            return nome;
+        }
+
+        public int ReturnPrezzo()
+        {
+            return prezzo;
+        }
+    }
+}
---
+++ Classi_lavoro/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("Classi_lavoro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Classi_lavoro")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("e83d75bd-37d2-4f8d-b437-4394622b7bdf")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+// usando l'asterisco '*' come illustrato di seguito:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
